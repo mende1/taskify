ea9ed0c0d2cbc5534b68b0847df1676ca518d1b5
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2023-11-30 11:19:31 -0300

    edit and destroy tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,34 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Task was successful updated" }
+      else
+        format.html { redirect_to :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.destroy
+        format.html { redirect_to tasks_url, notice: "Task was successful destroyed" }
+      else
+        format.html { redirect_to tasks_url, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def toggle
     @task = Task.find(params[:id])
     @task.update(completed: params[:completed])
